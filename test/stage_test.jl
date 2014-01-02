7c9258a23f8444412d15dfa5ab901660682fba20
Salt Pork <swadeless@gmail.com>
2014-01-02 09:21:15 -0500

    - update stage tests

--- test/stage_test.jl
+++ test/stage_test.jl
@@ -12,10 +12,14 @@ end
   sleep(2)
   a * b
 end
+@stage function sub(a, b)
+  sleep(2)
+  a - b
+end
 
 @debug(l, "initiating test")
-res_future = mult("mult", add("add", 1, 2), 3)
-res2 = mult("mult2", 2, 3)
+res2 = sub("sub2", sub("sub", mult("mult", 4, 3), 2), 2)
+res_future = add("add", mult("mult2", 1, 2), 3)
 @debug(l, "mult result: $(fetch(res2))")
 @debug(l, "madd result: $(fetch(res_future))")
 @debug(l, "completed test")
